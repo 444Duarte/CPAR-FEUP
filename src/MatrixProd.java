2983a9401cf756534e169c641d047cb3ff7ca261
Duarte <444Duarte>
2017-03-12 13:34:10 +0000

    exercise 1 java code finished

--- src/MatrixProd.java
+++ src/MatrixProd.java
@@ -1,12 +1,79 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class MatrixProd{
-	void lineMultiplication(int lines, int collumns){
-		
+	static void onMultLine(int m_ar, int m_br){
+
 	}
 
+	static void onMult(int m_ar, int m_br){
+
+		double temp;
+		long init, end, elapsedTime;
+		int i,j,k;
+
+		double[] pha = new double[m_ar*m_ar];
+		double[] phb = new double[m_ar*m_ar];
+		double[] phc = new double[m_ar*m_ar];
+
+		for(i=0; i<m_ar; i++)
+			for(j=0; j<m_ar; j++)
+				pha[i*m_ar + j] = (double)1.0;
+
+
+
+		for(i=0; i<m_br; i++)
+			for(j=0; j<m_br; j++)
+				phb[i*m_br + j] = (double)(i+1);
+
+
+		init = System.nanoTime();
+
+		for(i=0; i<m_ar; i++)
+		{	for( j=0; j<m_br; j++)
+		{	temp = 0;
+			for( k=0; k<m_ar; k++)
+			{
+				temp += pha[i*m_ar+k] * phb[k*m_br+j];
+			}
+			phc[i*m_ar+j]=temp;
+		}
+		}
+
+		end = System.nanoTime();
+		elapsedTime = end-init;
+
+		System.out.println("Time: "+((double)elapsedTime / 1000000000.0)+" seconds");
+	}
 
 	public static void main(String[] args){
-			
+		
+		System.out.println("1. Multiplication");
+		System.out.println("2. Line Multiplication");
+		System.out.print("Selection?: ");
+
+		Scanner scanner = new Scanner(System.in);
+
+		int opt = scanner.nextInt();
+		System.out.print("Dimensions: lins cols ? ");
+		int lines = scanner.nextInt();
+		int cols = scanner.nextInt();
+
+
+
+		switch (opt){
+			case 1:
+				onMult(lines, cols);
+				break;
+			case 2:
+				onMultLine(lines,cols);
+				break;
+			default:
+				System.out.println("Error");
+		}
+
+
 	}		
 }
 
